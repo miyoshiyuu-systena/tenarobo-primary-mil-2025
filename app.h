db7b6d249fc25187988e5fb1a347ec6583647f44
yutomiyoshi <seanayuuto0826@gmail.com>
2025-05-18 15:03:49 +0900

    [add]初期化タスクを作成

--- app.h
+++ app.h
@@ -2,26 +2,25 @@
 extern "C" {
 #endif
 
-#include "spikeapi.h"
+#include    "spikeapi.h"
+#include    "InitTask.h"
   
 /* タスク優先度 */
-#define MAIN_PRIORITY    (TMIN_APP_TPRI + 1) /* メインタスク */
-#define TRACER_PRIORITY  (TMIN_APP_TPRI + 2) /* ライントレースタスク */
+#define     INIT_PRIORITY       (TMIN_APP_TPRI + 1)
+#define     PERC_PRIORITY       (TMIN_APP_TPRI + 2)
+#define     DRIV_PRIORITY       (TMIN_APP_TPRI + 3)
+#define     DECI_PRIORITY       (TMIN_APP_TPRI + 4)
 
-/* タスク周期の定義 */
-#define LINE_TRACER_PERIOD  (100 * 1000) /* ライントレースタスク:100msec周期 */
+#ifndef     STACK_SIZE
+#define     STACK_SIZE          (4096)
+#endif      /* STACK_SIZE */
 
-
-#ifndef STACK_SIZE
-#define STACK_SIZE      (4096)
-#endif /* STACK_SIZE */
-
-#ifndef TOPPERS_MACRO_ONLY
+#ifndef     TOPPERS_MACRO_ONLY
 
 extern void main_task(intptr_t exinf);
 extern void tracer_task(intptr_t exinf);
 
-#endif /* TOPPERS_MACRO_ONLY */
+#endif      /* TOPPERS_MACRO_ONLY */
 
 #ifdef __cplusplus
 }
---
+++ task/InitTask.cpp
@@ -0,0 +1,25 @@
+#include    "InitTask.h"
+
+/**
+ * 初期化タスク
+ * @param   exinf     拡張情報
+ */
+void    init_task(intptr_t exinf)   {
+    /**
+     * センサーデータ取得インスタンスの生成
+     */
+    //  TODO miyoshi: センサーの初期化
+
+    /**
+     * シナリオチェーンの構成
+     */
+    //  TODO miyoshi: シナリオチェーンの構成
+
+    /**
+     * 駆動系インスタンスの生成
+     */
+    //  TODO miyoshi: 駆動系インスタンスの生成
+
+    //  タスク終了
+    ext_tsk();
+}
