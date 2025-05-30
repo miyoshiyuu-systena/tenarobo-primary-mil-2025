ba09e296ee8dfaa95dd95b1e9ed8b5e3033caa83
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-05-30 09:10:54 +0100

    [update]方向転換アクションの回転刻みを最適化（とは言っても画像処理含めてのチューニングはまだ）

--- action/ChangeDirectionAction.cpp
+++ action/ChangeDirectionAction.cpp
@@ -24,15 +24,31 @@ ActionCall changeDirectionActionFactory(
         }
 
         do {
+            /**
+             * 回転速度45°/sで200ms回転する
+             * これにより
+             * 45°/s * 0.2s = 9°
+             * 9°刻みでゲートを分析する
+             */
             if (is_clockwise) {
-                device->twinWheelDrive.leftSpinTurn(90);
+                device->twinWheelDrive.leftPivotTurn(45);
             } else {
-                device->twinWheelDrive.rightSpinTurn(90);
+                device->twinWheelDrive.rightPivotTurn(45);
             }
-            dly_tsk(500 * 1000);
-            
+            dly_tsk(200 * 1000);
+
+            /**
+             * 一時停止し、カメラの手ぶれなしで画像を取得する機会を与える
+             */
             device->twinWheelDrive.stop();
-            dly_tsk(500 * 1000);
+            dly_tsk(200 * 1000);
+
+            /**
+             * XXX
+             * 1サイクル400msなので、十分画像処理可能
+             * このスピードで撮像して、手ぶれがないかどうかは要検証
+             * 手ぶれが全くないならばstopしなくていいかもしれない
+             */
 
             for (ICloser* closer : closers) {
                 if (closer->isClosed())
