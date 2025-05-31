8e53ec68a889876a8a6f9148cbd6c42b28a48a08
miyoshiyuu <miyoshiyuu@systena.co.jp>
2025-05-31 14:32:10 +0900

    [debug]

--- task/DrivingTask.cpp
+++ task/DrivingTask.cpp
@@ -1,3 +1,4 @@
+#include    <math.h>
 #include    "syssvc/syslog.h"
 #include    "Motor.h"
 #include    "DrivingTask.h"
@@ -11,30 +12,33 @@ Motor rightWheel(   EPort::PORT_A,      Motor::EDirection::CLOCKWISE,           
 Motor leftWheel (   EPort::PORT_B,      Motor::EDirection::COUNTERCLOCKWISE,    true        );
 Motor frontArm  (   EPort::PORT_C,      Motor::EDirection::CLOCKWISE,           true        );
 
-const int8_t pwm = 50;
+const int8_t num = 50;
+int8_t count = 0;
 
 /**
  * 駆動指示
  * @param   exinf     拡張情報
  */
 void    driv_task(intptr_t exinf)   {
+    count++;
+
     /**
      * 右モーターへの指示
      */
     //  TODO miyoshi: 右モーターへの指示
-    rightWheel.setPower(pwm);
+    rightWheel.setPower(num);
 
     /**
      * 左モーターへの指示
      */
     //  TODO miyoshi: 左モーターへの指示
-    leftWheel.setPower(pwm);
+    leftWheel.setSpeed(num);
 
     /**
      * アームモータへの指示
      */
     //  TODO miyoshi: アームモータへの指示
-    frontArm.setPower(pwm);
+    frontArm.setPower(num * pow(-1, count));
 
     /**
      * 待機
