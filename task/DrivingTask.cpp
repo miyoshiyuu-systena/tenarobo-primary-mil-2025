4d587a969b51e14334b118a3a74fe3e676ba52c3
miyoshiyuu <miyoshiyuu@systena.co.jp>
2025-05-31 13:46:20 +0900

    [add]各種モーターの宣言と指示

--- task/DrivingTask.cpp
+++ task/DrivingTask.cpp
@@ -1,5 +1,17 @@
 #include    "syssvc/syslog.h"
+#include    "Motor.h"
 #include    "DrivingTask.h"
+
+using   namespace   spikeapi;
+
+// モーターの宣言
+// TODO: Motor::hasErrorでインスタンスの可否を判定できるので、初期化時に確認する
+/**                 SPIKE_port          Rotation_direction                      Count_reset */
+Motor rightWheel(   EPort::PORT_A,      Motor::EDirection::CLOCKWISE,           true        );
+Motor leftWheel (   EPort::PORT_B,      Motor::EDirection::COUNTERCLOCKWISE,    true        );
+Motor frontArm  (   EPort::PORT_C,      Motor::EDirection::CLOCKWISE,           true        );
+
+const int8_t pwm = 50;
 
 /**
  * 駆動指示
@@ -10,16 +22,19 @@ void    driv_task(intptr_t exinf)   {
      * 右モーターへの指示
      */
     //  TODO miyoshi: 右モーターへの指示
+    rightWheel.setPower(pwm);
 
     /**
      * 左モーターへの指示
      */
     //  TODO miyoshi: 左モーターへの指示
+    leftWheel.setPower(pwm);
 
     /**
      * アームモータへの指示
      */
     //  TODO miyoshi: アームモータへの指示
+    fromtArm.setPower(pwm);
 
     /**
      * 待機
