08b3d911c25561f65f3ad5bc0ac780a0fcba5817
miyoshiyuu <miyoshiyuu@systena.co.jp>
2025-05-31 15:03:17 +0900

    [add]力学センサーデータの読み出し

--- task/PerceptionTask.cpp
+++ task/PerceptionTask.cpp
@@ -1,5 +1,12 @@
 #include    "syssvc/syslog.h"
+#include    "ForceSensor.h"
 #include    "PerceptionTask.h"
+
+using   namespace   spikeapi;
+
+// 力学センサーの宣言
+//                          SPIKE_port
+ForceSensor forceSensor(    EPort::PORT_D      );
 
 /**
  * 知覚処理
@@ -10,6 +17,27 @@ void    perc_task(intptr_t exinf)   {
      * カメラデータの取得
      */
     //  TODO    miyoshi: カメラデータの取得
+
+    /**
+     * 力学センサデータの取得
+     */
+    syslog(LOG_INFO, "~~~~~~~~~~~~~~~~~~");
+    
+    float force, distance;
+    bool isPress, isTouch;
+    force = forceSensor.getForce();
+    syslog(LOG_INFO, "force %f [N]", force);
+
+    distance = forceSensor.getDistance();
+    syslog(LOG_INFO, "distance %f [mm]", distance);
+
+    isPress = forceSensor.isPress(0.05);
+    syslog(LOG_INFO, "press?? %d", isPress);
+
+    isTouch = forceSensor.isTouch();
+    syslog(LOG_INFO, "touch?? %d", isTouch);
+
+    syslog(LOG_INFO, "~~~~~~~~~~~~~~~~~~");
 
     /**
      * カラーセンサデータの取得
