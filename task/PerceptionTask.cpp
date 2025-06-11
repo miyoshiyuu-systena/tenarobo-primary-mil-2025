55e03b8951ec69438308bdb7c6a2f92bf44bb852
miyoshiyuu <miyoshiyuu@systena.co.jp>
2025-06-11 12:47:54 +0900

    [add]カラーセンサデータの取得

--- task/PerceptionTask.cpp
+++ task/PerceptionTask.cpp
@@ -1,5 +1,13 @@
+#include    <string.h>
 #include    "syssvc/syslog.h"
+#include    "ColorSensor.h"
 #include    "PerceptionTask.h"
+
+using   namespace   spikeapi;
+
+// カラーセンサの取得
+//                          SPIKE_port
+ColorSensor colorSensor(    EPort::PORT_F   );
 
 /**
  * 知覚処理
@@ -15,6 +23,24 @@ void    perc_task(intptr_t exinf)   {
      * カラーセンサデータの取得
      */
     //  TODO    miyoshi: カラーセンサデータの取得
+
+    syslog(LOG_WARNING, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    ColorSensor::RGB rgb;
+    ColorSensor::HSV hsv;
+    char rgb_buffer[50], hsv_buffer[50];
+
+    colorSensor.getRGB(&rgb);
+    colorSensor.getHSV(&hsv);
+    
+    sprintf(rgb_buffer, "RGB: %d, %d, %d", rgb.r, rgb.g, rgb.b);
+    syslog(LOG_WARNING, rbg_buffer);
+
+    sprintf(hsv_buffer, "HSV: %d, %d, %d", hsv.h, hsv.s, hsv.v);
+    syslog(LOG_WARNING, hsv_buffer);
+
+    syslog(LOG_WARNING, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
 
     /**
      * 超音波センサデータの取得
