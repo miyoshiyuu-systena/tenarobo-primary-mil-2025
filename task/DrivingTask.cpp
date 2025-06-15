947dc2a7a4a1158f10b98c2ec56c059a5617d814
miyoshiyuu <miyoshiyuu@systena.co.jp>
2025-06-15 15:51:30 +0900

    [debug]

--- task/DrivingTask.cpp
+++ task/DrivingTask.cpp
@@ -8,12 +8,17 @@
  * @param   exinf     拡張情報
  */
 void    driv_task(intptr_t exinf)   {
-    auto file_logger = spdlog::basic_logger_mt("my_app_logger", "logs/application.log", true);
-    file_logger->set_level(spdlog::level::info);
-    file_logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%L] [%n] %v");
-    file_logger->info("アプリケーションを開始します。");
-    file_logger->warn("設定ファイルが見つかりませんでした。デフォルト値を使用します。");
-    file_logger->error("処理中にエラーが発生しました: {}", "無効な入力");
+    try {
+        auto file_logger = spdlog::basic_logger_mt("my_app_logger", "logs/application.log", true);
+        file_logger->set_level(spdlog::level::info);
+        file_logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%L] [%n] %v");
+        file_logger->info("アプリケーションを開始します。");
+        file_logger->warn("設定ファイルが見つかりませんでした。デフォルト値を使用します。");
+        file_logger->error("処理中にエラーが発生しました: {}", "無効な入力");
+        spdlog::shutdown();
+    } catch (const spdlog::spdlog_ex& ex) {
+        ;
+    }
     /**
      * 右モーターへの指示
      */
