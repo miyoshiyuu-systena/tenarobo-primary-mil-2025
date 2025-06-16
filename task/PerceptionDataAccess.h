335d500932287fc0b43d0214049381c4d7e71b64
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-16 10:46:39 +0100

    [update]知覚データ共有

--- task/PerceptionDataAccess.h
+++ task/PerceptionDataAccess.h
@@ -3,6 +3,31 @@
 
 struct PerceptionDataAccess
 {
+    /**
+     * 力センサーの値
+     */
+    float force;
+
+    /**
+     * 超音波センサーの値
+     */
+    float distance;
+
+    /**
+     * カラーセンサーの値
+     */
+    int color[3];
+
+    /**
+     * 左モーターの値
+     */
+    int leftMotor;
+
+    /**
+     * 右モーターの値
+     */
+    int rightMotor;
+
     // ここに知覚データのアクセスを定義する
 };
 
