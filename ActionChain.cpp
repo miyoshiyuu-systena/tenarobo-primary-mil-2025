184435b6f4a9367f1e933a10eb120bb757fc7347
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-16 10:35:29 +0100

    [add]アクションチェーンクラス

---
+++ ActionChain.cpp
@@ -0,0 +1,52 @@
+#include "ActionChain.h"
+#include "Logger.h"
+
+using   namespace   spikeapi;
+
+ActionChain::ActionChain(
+    TwinWheelDrive* twinWheelDrive,
+    Motor* frontArm,
+    PercDataAccess& percDataAccess,
+    void (*actionCall)(void),
+    std::string actionName
+)
+    : mTwinWheelDrive(twinWheelDrive)
+    , mFrontArm(frontArm)
+    , mPercDataAccess(&percDataAccess)
+    , mActionCall(actionCall)
+    , mActionName(actionName)
+    , mNextAction(nullptr)
+    , mIsEnd(false)
+{}
+
+ActionChain::~ActionChain()
+{
+    Logger::getInstance().logInfo("ActionChain %s 終了", mActionName.c_str());
+}
+
+void ActionChain::execute()
+{
+    mActionCall();
+    mIsEnd = true;
+}
+
+bool ActionChain::isEnd()
+{
+    return mIsEnd;
+}
+
+void ActionChain::setNext(ActionChain* nextAction)
+{
+    mNextAction = nextAction;
+}
+
+void ActionChain::deleteNext()
+{
+    if (mNextAction == nullptr)
+    {
+        return;
+    }
+    mNextAction->deleteNext();
+    delete mNextAction;
+    mNextAction = nullptr;
+}
---
+++ ActionChain.h
@@ -0,0 +1,88 @@
+#ifndef _ACTION_CHAIN_H_
+#define _ACTION_CHAIN_H_
+
+#include "TwinWheelDrive.h"
+#include "Motor.h"
+#include "PercDataAccess.h"
+#include <string>
+
+class ActionChain
+{
+public:
+    /**
+     * コンストラクタ
+     * @param twinWheelDrive ツインホイールドライブのポインタ
+     * @param frontArm 前腕モーターのポインタ
+     * @param percDataAccess 知覚データへのアクセス（センサデータ）
+     * @param actionCall アクションの実行
+     * @param actionName アクションの名前（ログ出力のため）
+     */
+    ActionChain(
+        TwinWheelDrive* twinWheelDrive,
+        Motor* frontArm,
+        PercDataAccess& percDataAccess,
+        void (*actionCall)(void),
+        std::string actionName
+    );
+
+    /**
+     * デストラクタ
+     * アクションの完全終了を宣言する
+     */
+    ~ActionChain();
+
+    /**
+     * アクションの実行
+     */
+    void execute();
+
+    /**
+     * アクションが終了したか
+     */
+    bool isEnd();
+
+    /**
+     * 次のアクションの設定
+     */
+    void setNext(ActionChain* nextAction);
+
+    /**
+     * 次以降のアクションの削除
+     */
+    void deleteNext();
+
+private:
+    /**
+     * 周辺モジュールへのアクセス
+     */
+    TwinWheelDrive* mTwinWheelDrive;
+    Motor* mFrontArm;
+
+    /**
+     * 知覚データへのアクセス
+     */
+    PercDataAccess* mPercDataAccess;
+    
+    /**
+     * アクションの名前
+     */
+    std::string mActionName;
+
+    /**
+     * アクションの実行
+     */
+    void (*mActionCall)(void);
+
+
+    /**
+     * 次のアクション
+     */
+    ActionChain* mNextAction;
+    
+    /**
+     * このアクションチェーンが終了している
+     */
+    bool mIsEnd;
+};
+
+#endif // _ACTION_CHAIN_H_
