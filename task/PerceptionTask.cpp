edb0cbe577f122a3693a3244eac49c586b346b67
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-16 16:48:03 +0100

    [update]ロガーに知覚データを蓄積

--- task/PerceptionTask.cpp
+++ task/PerceptionTask.cpp
@@ -2,6 +2,7 @@
 #include    "PerceptionTask.h"
 #include    "share/PerceptionDataAccess.h"
 #include    "share/ModuleAccess.h"
+#include    "logging/Logger.h"
 #include    <string>
 
 /**
@@ -44,11 +45,11 @@ void    perc_task(intptr_t exinf)   {
                             std::to_string(perceptionDataAccess.color[0]) + "," + 
                             std::to_string(perceptionDataAccess.color[1]) + "," + 
                             std::to_string(perceptionDataAccess.color[2]);
-    syslog(LOG_WARNING, sensorsStr.c_str());
+    Logger::getInstance().logWarning(sensorsStr);
     
     std::string motorsStr = "Motors - Left:" + std::to_string(perceptionDataAccess.leftMotor) + 
                            " Right:" + std::to_string(perceptionDataAccess.rightMotor);
-    syslog(LOG_WARNING, motorsStr.c_str());
+    Logger::getInstance().logWarning(motorsStr);
 
     //  タスク終了
     ext_tsk();
