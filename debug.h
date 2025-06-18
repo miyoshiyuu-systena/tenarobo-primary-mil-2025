6d1dc8683e0bac11ab5e9c4dd05e4a450ca5678e
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-18 14:35:08 +0100

    [add]デバッグ用切り替えの強化

--- debug.h
+++ debug.h
@@ -2,9 +2,28 @@
 #define _DEBUG_H_
 
 /**
+ * デバッグ動作の有効/無効
+ * 
+ * @note
+ *  コンパイルする前に、各変数の値を調整して、記録を残したりできる
+ */
+
+/**
  * 知覚タスクのログ出力の有効/無効
  * デバッグ用
  */
 const bool isPerceptionLoggingEnable = false;
 
-#endif
+/**
+ * 知覚タスクをログ出力するとき、マスクを無視して全てのデータを取得するか
+ * デバッグ用
+ */
+const bool isPerceptionLoggingIgnoreMask = isPerceptionLoggingEnable && false;
+
+/**
+ * Webカメラの画像を保存するか
+ * デバッグ用
+ */
+const bool isSaveCameraImage = false;
+
+#endif // _DEBUG_H_
