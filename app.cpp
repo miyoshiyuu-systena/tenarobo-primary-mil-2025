5abf6f45bd48be69952e60f23bdbdd1e6667b74a
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-20 19:01:19 +0100

    [update][debug]超早い！！

--- app.cpp
+++ app.cpp
@@ -59,7 +59,7 @@ void main_task(intptr_t exinf)   {
         laneTracingAssistGenerator(
             false,
             100.0f,
-            1.0f,
+            0.5f,
             10.0f,
             calcBlackWhiteBorderError
         )
@@ -71,7 +71,7 @@ void main_task(intptr_t exinf)   {
         "action1: 白黒の直線に沿って走行し、曲がり角に到達したら終了",
         &device,
         goStraightActionFactory(
-            400.0f,
+            500.0f,
             10,
             assistGenerators1,
             closerGenerators1
