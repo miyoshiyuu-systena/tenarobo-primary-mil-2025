341d6c7902ccbf436dc37187c9b2c280e7d53d9e
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-21 18:20:45 +0100

    [delete]不要なクローザー

--- app.cpp
+++ app.cpp
@@ -20,7 +20,6 @@
 #include "BlackFloorCloser.h"
 #include "StraightCloser.h"
 #include "StraightStrictCloser.h"
-#include "WhiteFloorAndStraightStrictCloser.h"
 #include "CurveCloser.h"
 #include "TimedCloser.h"
 #include "OnRightEdgeCloser.h"
