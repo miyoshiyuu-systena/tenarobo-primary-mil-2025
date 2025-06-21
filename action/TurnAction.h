8fe1985a958f348520a6ae1e30e85a0bcb27d459
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-21 02:55:46 +0100

    [add]ピボットターンのアクション

---
+++ action/TurnAction.h
@@ -0,0 +1,35 @@
+#ifndef TURN_ACTION_H
+#define TURN_ACTION_H
+
+#include "ActionNode.h"
+#include "ICloserGenerator.h"
+#include <vector>
+
+/**
+ * その場で回転アクションのファクトリー関数
+ * @return 回転アクション
+ */
+ActionCall turnActionFactory(
+    /**
+     * その場回転速度[°/s]
+     */
+    float angular_speed,
+
+    /**
+     * 回転方向（時計回りか反時計回りか）
+     */
+    bool is_clockwise,
+
+    /**
+     * 検知間隔[ms]
+     */
+    int detectInterval,
+
+    /**
+     * 終了判定ジェネレータのリスト（複数の終了判定を順次適用）
+     * 何を基準にして動作を止めるか
+     */
+    std::vector<ICloserGenerator> closerPtrGenerators
+);
+
+#endif
