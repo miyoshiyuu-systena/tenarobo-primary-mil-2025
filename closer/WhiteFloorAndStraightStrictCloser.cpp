3b6a572145dcddf48a192a04538a6723f3e27db5
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-21 12:24:36 +0100

    [add]白い床と直線を検知するクローザーの実装(未完成)

---
+++ closer/WhiteFloorAndStraightStrictCloser.cpp
@@ -0,0 +1,38 @@
+#include "WhiteFloorAndStraightStrictCloser.h"
+
+ICloserGenerator whiteFloorAndStraightStrictGenerator() {
+    return []() -> ICloser* {
+        return new WhiteFloorAndStraightStrict();
+    };
+}
+
+WhiteFloorAndStraightStrict::WhiteFloorAndStraightStrict() : ICloser()
+{
+    mask = PERCEPTION_REPORT_MASK_COLOR;
+}
+
+WhiteFloorAndStraightStrict::~WhiteFloorAndStraightStrict()
+{
+}
+
+void WhiteFloorAndStraightStrict::init()
+{
+}
+
+bool WhiteFloorAndStraightStrict::isClosed(PerceptionReport* report)
+{
+    static int H_UPPER_THRESHOLD = 360;
+    static int H_LOWER_THRESHOLD = 0;
+    static int S_UPPER_THRESHOLD = 100;
+    static int S_LOWER_THRESHOLD = 0;
+    static int V_UPPER_THRESHOLD = 100;
+    static int V_LOWER_THRESHOLD = 70;
+
+    // XXX　とりあえず色だけで終了判定
+    
+    return (
+        (H_LOWER_THRESHOLD <= report->h && report->h <= H_UPPER_THRESHOLD) &&
+        (S_LOWER_THRESHOLD <= report->s && report->s <= S_UPPER_THRESHOLD) &&
+        (V_LOWER_THRESHOLD <= report->v && report->v <= V_UPPER_THRESHOLD)
+    );
+}
---
+++ closer/WhiteFloorAndStraightStrictCloser.h
@@ -0,0 +1,26 @@
+#ifndef _WHITE_FLOOR_AND_STRAIGHT_STRICT_CLOSER_H_
+#define _WHITE_FLOOR_AND_STRAIGHT_STRICT_CLOSER_H_
+
+#include "ICloser.h"
+#include "ICloserGenerator.h"
+
+/**
+ * 白い床と目前の直線を両方検知したら終了判定を出すクラスのファクトリー関数
+ * @return 白い床と目前の直線を両方検知したら終了判定を出すクラスのファクトリー関数
+ */
+ICloserGenerator whiteFloorAndStraightStrictGenerator();
+
+/**
+ * 白い床と目前の直線を両方検知したら終了判定を出すクラス
+ */
+class WhiteFloorAndStraightStrict : public ICloser
+{
+    public:
+        WhiteFloorAndStraightStrict();
+        ~WhiteFloorAndStraightStrict();
+
+        void init() override;
+        bool isClosed(PerceptionReport* report) override;
+};
+
+#endif
