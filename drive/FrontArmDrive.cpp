4cf856285e404c2766dbe852db6bcb300e6e61d9
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-22 01:00:44 +0100

    [add]前腕のインターフェースを充実

--- drive/FrontArmDrive.cpp
+++ drive/FrontArmDrive.cpp
@@ -18,3 +18,23 @@ float FrontArmDrive::getSpeed() const
 {
     return mMotor.getSpeed();
 }
+
+int FrontArmDrive::getCount() const
+{
+    return mMotor.getCount();
+}
+
+void FrontArmDrive::resetCount()
+{
+    mMotor.resetCount();
+}
+
+void FrontArmDrive::hold()
+{
+    mMotor.hold();
+}
+
+bool FrontArmDrive::isStalled() const
+{
+    return mMotor.isStalled();
+}
