baf0f5f78b16ab3eba55b30242aaa6051a60f8f7
yuto-miyoshi <seanayuuto0826@gmail.com>
2025-06-22 13:36:53 +0100

    [add]画像取得タスク

--- app.h
+++ app.h
@@ -6,6 +6,7 @@ extern "C" {
   
 /* タスク優先度 */
 #define     MAIN_PRIORITY       (TMIN_APP_TPRI + 1)
+#define     IMAGE_ACQ_PRIORITY  (TMIN_APP_TPRI + 2)  // 画像取得タスク（高優先度）
 
 #ifndef     STACK_SIZE
 #define     STACK_SIZE          (4096)
@@ -14,6 +15,7 @@ extern "C" {
 #ifndef     TOPPERS_MACRO_ONLY
 
 extern void main_task(intptr_t exinf);
+extern void image_acquisition_task(intptr_t exinf);
 
 #endif      /* TOPPERS_MACRO_ONLY */
 
