4799b1d9b3604272b07f799d10047a0154279048
miyoshiyuu <miyoshiyuu@systena.co.jp>
2025-07-15 12:42:30 +0900

    [refactor]コメントアウトなど

--- action/LineTraceAction.cpp
+++ action/LineTraceAction.cpp
@@ -16,6 +16,20 @@ using   namespace   spikeapi;
  * @note
  *      実験的に確定すること
  *      輝度の値は大会会場の光学環境と床の材料に影響を受ける
+ * 
+ * @note
+ *      輝度とは
+ *      面積のある光源を観測者側から見たときの眩しさを示す単位です。
+ *      ディスプレイなどの光源や照明の明るさの指標として使われることが多く、輝度が高いほど明るくなり、輝度が低いほど輝度の値は低くなります。
+ *      
+ *      ライントレースにおける輝度の有効性
+ *      黒い素材は光を吸収し、白い素材は光を反射する性質がある。
+ *      すなわち、黒い素材は輝度が低くなり、白い素材は輝度が大きくなる。
+ *      これを利用して、床面のグレースケールを判定する
+ */
+/**
+ * HACK: 輝度を使用しないで済むかも
+ * そもそも輝度を使用せずに、HSV値を使用して判定できる可能性あり。
  */
 const float THRESHOLD_DEFAULT = 40.0f;
 
@@ -29,15 +43,33 @@ const float THRESHOLD_DEFAULT = 40.0f;
 const float Kp = 0.5f;
 
 /**
- * この関数ではラインの右縁を通るように、ライントレースする
+ * ライントレースアクションを生成するファクトリー関数
+ * @note
+ *      この関数で作成したアクションでは、ロボットが黒いラインの右縁に沿って移動する
  */
 std::function<void(ActionNode*&)> line_trace_action(float speed, int duration, int vacation_duration, std::function<bool()> judge)
 {
     return [speed, duration, vacation_duration, judge](ActionNode*& next_ptr) {
         do {
-            // P制御による速度制御
+            /**
+             * P制御による駆動指示
+             * errorは、実測した輝度が閾値よりもどのくらい明るかったかを表す。
+             * (負数のときは閾値よりも暗かったことを表している。)
+             * 
+             * errorが正数のときは目標よりも白いところを通っているため、右輪を加速して左方向に移動する
+             * errorが負数のときは目標よりも黒いところを通っているため、左輪を加速して右方向に移動する
+             * この細かい調整により、黒い線の右縁を左右中心に移動するようにロボットの動きを最適化している
+             * 
+             * @note 
+             *      P制御とは
+             *      制御対象の目標値と現在の値の偏差（誤差）に比例した操作量を出す制御方式
+             *      今回の場合、比例係数Kpを使用している
+             */
             const float error = perceptionDataAccess.brightness - THRESHOLD_DEFAULT;
-            twinWheelDrive.setSpeed(speed - Kp * error, speed + Kp * error);
+            twinWheelDrive.setSpeed(
+                speed - Kp * error,     //  左輪
+                speed + Kp * error      //  右輪
+            );
 
             // 判定周期
             dly_tsk(duration * 1000);
