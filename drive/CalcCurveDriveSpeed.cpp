16a03e927a4252b8b34f99a9dc6c469de589c449
taro <tarohuygens0826@icloud.com>
2025-07-23 23:46:48 +0900

    [add] 新しい関数 calcCurveSpeedsByLinearSpeed を追加し、線速度に基づく曲線走行の速度計算を実装

--- drive/CalcCurveDriveSpeed.cpp
+++ drive/CalcCurveDriveSpeed.cpp
@@ -31,3 +31,33 @@ void calcCurveSpeedsByAngularSpeed(float angular_speed, float radius, float spee
     speeds[0] = outer_speed;
     speeds[1] = inner_speed;
 }
+
+void calcCurveSpeedsByLinearSpeed(float linear_speed, float radius, float speeds[2])
+{
+    // 半径が車輪間隔の半分より小さい場合は停止
+    if (radius < WHEEL_TREAD_MM / 2.0f) {
+        speeds[0] = 0.0f;
+        speeds[1] = 0.0f;
+        return;
+    }
+
+    // 左曲線：右輪が外輪、左輪が内輪
+    float outer_radius = radius + WHEEL_TREAD_MM / 2.0f;
+    float inner_radius = radius - WHEEL_TREAD_MM / 2.0f;
+
+    // 中心の線速度から外輪・内輪の線速度を計算
+    float outer_speed_mms = linear_speed * outer_radius / radius;
+    float inner_speed_mms = linear_speed * inner_radius / radius;
+
+    // mm/sから°/sに変換
+    float outer_speed = outer_speed_mms * 360.0f / (M_PI * WHEEL_DIAMETER_MM);
+    float inner_speed = inner_speed_mms * 360.0f / (M_PI * WHEEL_DIAMETER_MM);
+
+    // 内輪の速度が負になる場合は0にする
+    if (inner_speed < 0.0f) {
+        inner_speed = 0.0f;
+    }
+
+    speeds[0] = outer_speed;
+    speeds[1] = inner_speed;
+}
--- drive/CalcCurveDriveSpeed.h
+++ drive/CalcCurveDriveSpeed.h
@@ -5,10 +5,20 @@
  * 曲線走行の速度を計算する
  * @param angular_speed 角速度(°/s)
  * @param radius 半径(mm)
- * @param speeds 速度(mm/s)を格納する配列（[0]: 外輪、[1]: 内輪）
+ * @param speeds 車輪回転速度(°/s)を格納する配列（[0]: 外輪、[1]: 内輪）
  * @note
  *  2輪の回転速度に差をつけて走行することで、曲線走行を実現する
  */
  void calcCurveSpeedsByAngularSpeed(float angular_speed, float radius, float speeds[2]);
+
+/**
+ * 曲線走行の速度を計算する
+ * @param linear_speed 線速度(mm/s)
+ * @param radius 半径(mm)
+ * @param speeds 車輪回転速度(°/s)を格納する配列（[0]: 外輪、[1]: 内輪）
+ * @note
+ *  2輪の回転速度に差をつけて走行することで、曲線走行を実現する
+ */
+ void calcCurveSpeedsByLinearSpeed(float linear_speed, float radius, float speeds[2]);
 
 #endif // CALC_CURVE_DRIVE_SPEED_H
